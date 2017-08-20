3c009a107fde8f53f1a54bda843cade61ebd40a4
Judah Lamm <Judah.Lamm@gmail.com>
2017-08-20 18:01:50 -0400

    Add in logic to the extended kaman function

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -58,4 +58,44 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  //Lesson 5.14
+
+  #define PI 3.14159265
+
+  double px = x_[0];
+  double py = x_[1];
+  double vx = x_[2];
+  double vy = x_[3];
+
+  double rho = sqrt(px * px + py * py);
+  double phi = atan2(py, px);
+
+  if(rho < 1e-6) rho = 1e-6;
+  double rho_dot = (px * vx + py * vy) / rho;
+
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rho_dot;
+
+  VectorXd y = z - z_pred;
+
+  while(y[1] > PI || y[1] < -PI)
+  {
+    if(y[1] > PI)
+      y[1]-= PI;
+    else y[1]+= PI;
+  }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  // new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
 }
