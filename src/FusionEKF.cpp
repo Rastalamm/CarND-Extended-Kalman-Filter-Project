32f4257c0ed00e1beb2006ba7ec2552c2fe54484
Judah Lamm <Judah.Lamm@gmail.com>
2017-08-20 18:01:30 -0400

    Convert radar from polar to cartesian coordinates and initialize state for Radar data

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -99,6 +99,15 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+
+      float ro = measurement_pack.raw_measurements_[0];
+      float phi = measurement_pack.raw_measurements_[1];
+      float rho_dot = measurement_pack.raw_measurements_[2];
+
+      ekf_.x_[0] = ro * sin(phi);
+      ekf_.x_[1] = phi * cos(phi);
+      ekf_.x_[2] = rho_dot * cos(phi);
+      ekf_.x_[3] = 0;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
@@ -170,6 +179,11 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.R_ = R_radar_;
+
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
     // Lesson 5.13
